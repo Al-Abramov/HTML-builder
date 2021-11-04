cfc05309ff2ef4a2ecb067dc4b73d1e5c68df0b8
Alex <x3m.f@mail.ru>
2021-11-04 17:35:29 +0400

    feat: add script for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+
+
+const stdout = process.stdout;
+const stdin = process.stdin;
+
+const filePath = path.join(__dirname, 'text.txt');
+
+fs.open(filePath, 'w', err => {
+  if(err) {
+    throw err;
+  }
+});
+
+  stdin.on('data', (data) => {
+    if(data.toString().trim() === 'exit') {
+      stdout.write('Good luck');
+      process.exit();
+    }
+
+    stdout.write(`Hi ${data.toString()}`);
+   
+    fs.appendFile(filePath, data.toString(), err => {
+      if(err) {
+        throw err
+      }
+    })
+  });
+
+
+stdout.write('What is your name?');
+
+process.on('SIGINT', () => {
+  stdout.write('Good luck');
+  process.exit();
+});
